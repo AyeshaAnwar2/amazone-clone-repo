b9c1be2acea8dcc5251af87654b02ce6d218b578
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-03-14 20:39:04 +0500

    add now

---
+++ file1.js
@@ -0,0 +1,12 @@
+let namee = prompt("enter your name");
+console.log("hello" + namee);
+
+let age=27;
+console.log(age);
+
+if(age<18){
+    console.log("not eligible");
+}
+else{
+    console.log('no');
+}
